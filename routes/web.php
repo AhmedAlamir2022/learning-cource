ae9bc8a1f22b826957e3e3b2bd41dad1de099988
Ahmed Alamir <ahmedalamir521@gmail.com>
2024-11-22 16:19:53 +0300

    lec-2-routes

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,37 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/about', function () {
-    return view('about');
+    return 'about page';
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return 'contact page';
 });
+
+Route::post('/contact', function () {
+    return 'contact page';
+});
+
+Route::get('/blog', function () {
+    return 'blog page';
+});
+
+Route::get('/services', function () {
+    return 'services page';
+})->name('services');
+
+Route::get('/team', function () {
+    return 'our-team page';
+});
+
+Route::get('course/{name}/{hours?}', function ($name, $hours = 0) {
+    if($hours == 0){
+        return 'course name is '.$name;
+    }else{
+        return 'course name is '.$name . ' and hours is '.$hours;
+    }
+})->whereAlpha('name')->whereNumber('hours');
+
+
+
